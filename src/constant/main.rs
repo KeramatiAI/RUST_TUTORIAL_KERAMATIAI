27eb05b5271afb5773cfd3d01f54c880206507bc
keramatiai <davoud.keramati@gmail.com>
2026-08-25 17:16:34 +0330

    edit

--- src/constant/main.rs
+++ src/constant/main.rs
@@ -1,6 +1,7 @@
 // تعریف یک ثابت (معمولاً با حروف بزرگ و زیرخط)
 const SECONDS_IN_MINUTE: u32 = 60;
-
+const PI: f32 = 3.14;
 fn main() {
     println!("تعداد ثانیه در یک دقیقه: {}", SECONDS_IN_MINUTE);
+    println!("عدد پی: {}", PI);
 }
--- src/data_types/main.rs
+++ src/data_types/main.rs
@@ -1,22 +1,23 @@
 fn main() {
     // 1. اعداد صحیح (Integers)
     // می‌تواند با علامت (i) یا بدون علامت (u) باشد.
-    let integer: i32 = -42;
-    let unsigned_integer: u32 = 100;
+    // انواع عددی رایج: i32, i64, u32, u64 (i = signed, u = unsigned، عدد = تعداد بیت)
+    let integer: i32 = -42; // immutable
+    let unsigned_integer: u32 = 100; // immutable
     println!("عدد صحیح: {} و عدد صحیح بدون علامت: {}", integer, unsigned_integer);
 
     // 2. اعداد اعشاری (Floating-point)
     // f64 نوع پیش‌فرض است و دقت بالاتری دارد.
-    let float: f64 = 3.14;
+    let float: f64 = 3.14; // immutable
     println!("عدد اعشاری: {}", float);
 
     // 3. بولین (Boolean)
-    let is_rust_fun: bool = true;
+    let is_rust_fun: bool = true; // immutable
     println!("آیا راست جذاب است؟ {}", is_rust_fun);
 
     // 4. کاراکتر (Character)
     // در Rust کاراکترها با تک‌کویتی '' تعریف می‌شوند و 4 بایت هستند (Unicode)
-    let letter: char = 'R';
-    let emoji: char = '🚀';
+    let letter: char = 'R'; // immutable
+    let emoji: char = '🚀'; // immutable
     println!("حرف: {} و ایموجی: {}", letter, emoji);
 }
