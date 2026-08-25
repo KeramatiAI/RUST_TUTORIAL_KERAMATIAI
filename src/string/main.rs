fdfa8e285fd82e094a12a04a7768cc2997b70c3b
keramatiai <davoud.keramati@gmail.com>
2026-08-25 18:12:04 +0330

    Edit string directory

--- src/string/main.rs
+++ src/string/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // 1. نوع &str (String Slice)
     // این‌ها معمولاً رشته‌های ثابت هستند که در زمان کامپایل در حافظه برنامه قرار می‌گیرند.
-    let fixed_string: &str = "سلام، این یک رشته ثابت است";
+    let fixed_string: &str = "Hello this is a fixed string";
     println!("{}", fixed_string);
 
     // 2. نوع String (رشته داینامیک)
@@ -9,11 +9,11 @@ fn main() {
     let mut dynamic_string = String::from("Rust");
 
     // اضافه کردن یک کاراکتر یا رشته به انتهای آن
-    dynamic_string.push_str(" خیلی عالی است!");
+    dynamic_string.push_str(" Is Good.");
     println!("{}", dynamic_string);
 
     // 3. تبدیل &str به String و برعکس
-    let s1 = "تبدیل ثابت به داینامیک".to_string();
+    let s1 = "JAVA".to_string();
     let s2: &str = &dynamic_string; // گرفتن Slice از String (بسیار رایج)
 
     println!("s1: {}, s2: {}", s1, s2);
