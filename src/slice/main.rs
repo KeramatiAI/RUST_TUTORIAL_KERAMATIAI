52753971418bf8b029b93c1e050935c574eadea7
keramatiai <davoud.keramati@gmail.com>
2026-09-03 21:00:46 +0330

    Edit Slice

--- src/slice/main.rs
+++ src/slice/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let n1 = "Davoud Keramati is a programer".to_string();
-    println!("length of string is {}",n1.len());
-    let c1 = &n1[4..9];
+    let str = "Davoud Keramati is a programer".to_string();
+    println!("length of string is {}",str.len());
+    let slice = &str[4..9];
 
     // fetches characters at 4,5,6,7, and 8 indexes
-    println!("{}",c1);
+    println!("{}",slice);
 }
