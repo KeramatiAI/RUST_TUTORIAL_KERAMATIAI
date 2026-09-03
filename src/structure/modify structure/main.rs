0235c1144318df01544ea5a9f9f41fdddd5eb131
keramatiai <davoud.keramati@gmail.com>
2026-09-03 21:45:42 +0330

    Add modify structure

---
+++ src/structure/modify structure/main.rs
@@ -0,0 +1,14 @@
+struct Employee {
+    name:String,
+    company:String,
+    age:u32
+}
+fn main() {
+    let mut emp1 = Employee {
+        company:String::from("Davoud in a good boy."),
+        name:String::from("Davoud"),
+        age:50
+    };
+    emp1.age = 40;
+    println!("Name is :{} company is {} age is {}",emp1.name,emp1.company,emp1.age);
+}
