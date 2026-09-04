33991b825d24592c8cb8c28b43cd1fcc98a92dc2
keramatiai <davoud.keramati@gmail.com>
2026-09-04 17:50:03 +0330

    Edit modify structure

--- src/structure/modify structure/main.rs
+++ src/structure/modify structure/main.rs
@@ -1,14 +1,18 @@
+//Declaring a structure
 struct Employee {
     name:String,
     company:String,
     age:u32
 }
+
 fn main() {
+    //Initializing a structure
     let mut emp1 = Employee {
         company:String::from("OpenAI"),
         name:String::from("Davoud"),
         age:50
     };
+    //Modifying a struct instance
     emp1.age = 40;
     println!("Name is :{} company is {} age is {}",emp1.name,emp1.company,emp1.age);
 }
