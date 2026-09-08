793f58e73a5015a551c6b5ae98a0185361c0d748
keramatiai <davoud.keramati@gmail.com>
2026-09-08 22:05:14 +0330

    Update ownership --> sample_2

--- src/ownership/sample_2/main.rs
+++ src/ownership/sample_2/main.rs
@@ -3,3 +3,7 @@ fn main(){
     let y = x; // ✅ No problem! The numbers are Copy
     println!("{} , {}", x, y);
 }
+/*
+Simple types (number, bool, char, and tuple are among these types) are copied onto the stack
+— there's nothing on the heap to worry about.
+*/
