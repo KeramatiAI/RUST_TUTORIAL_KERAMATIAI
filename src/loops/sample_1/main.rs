5636d3923c665e365e6f7f0bf103ccf779cdc955
keramatiai <davoud.keramati@gmail.com>
2026-09-08 21:53:28 +0330

    Update loops --> sample_1

--- src/loops/sample_1/main.rs
+++ src/loops/sample_1/main.rs
@@ -1,4 +1,5 @@
 fn main(){
+    println!("------ loop ------");
     // loop: infinite loop with return value
     let mut hash_attempts = 0;
     let _found = loop {
@@ -7,19 +8,21 @@ fn main(){
             break hash_attempts; // loop can return value!
         }
     };
+    println!("found in:{}",_found);
 
+    println!("------ while ------");
     // while
     let mut n = 3;
     while n != 0 {
         println!("{}", n);
         n -= 1;
     }
-
+    println!("------ for ------");
     // for: most common and safest method
     for i in 0..5 { // 0,1,2,3,4
         println!("Block number {}", i);
     }
-
+    println!("------ for ------");
     for block in [10, 20, 30].iter() {
         println!("{}", block);
     }
